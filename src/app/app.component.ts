bb6f28904ba91ba34739f3f80beb82d1dfb21bfb
Mikkel-P <mikkelp.1993@gmail.com>
2023-11-07 23:25:34 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+// import { AuthenticationService } from 'src/services/authentication.service';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-components';
-}
+  title = 'Angular components';
+  // isLoggedIn$: Observable<boolean>;
+
+  constructor(/*private authService: AuthenticationService, */private router: Router, private matDialog: MatDialog) {
+    // this.isLoggedIn$ = this.authService.loggedIn$;
+    // this.authService.loggedIn$.subscribe(x => {
+    //   this.test = !x;
+    // })
+  };
+
+  Logout() {
+    // this.authService.removeToken();
+    this.router.navigateByUrl("/")
+  };
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,74 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar'
+import { MatTooltipModule } from '@angular/material/tooltip'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatStepperModule } from '@angular/material/stepper';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { AsyncPipe } from '@angular/common';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatDialogModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatNativeDateModule,
+    MatToolbarModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatTooltipModule,
+    MatProgressBarModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatDatepickerModule,
+    MatStepperModule,
+    ClipboardModule,
+    MatTreeModule,
+    MatAutocompleteModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatCardModule,
   ],
-  providers: [],
+  providers: [AsyncPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
